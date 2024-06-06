23dd77de398c27d87d1a79b667c643e8e91f76b3
Richie Holsenback <richieholsenback@gmail.com>
2024-06-06 16:46:48 -0400

    Game mechanics set, winning not yet set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player/Player.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handlePlayerChange() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player One" symbol="X" />
-          <Player initialName="Player Two" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player One"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player Two"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handlePlayerChange}
+          activePlayer={activePlayer}
+        />
       </div>
     </main>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayer }) {
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+
+  const handleSquareClick = (rowIndex, colIndex) => {
+    setGameBoard((prevBoard) => {
+      let newBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      newBoard[rowIndex][colIndex] = activePlayer;
+      return newBoard;
+    });
+    onSelectSquare();
+  };
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIdx) => (
-        <li key={rowIdx}>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
           <ol>
-            {row.map((playerSymbol, colIdx) => (
-              <li key={colIdx}>
-                <button>{playerSymbol}</button>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSquareClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
